96c3b5cd87bd540e5e7bf6bf967201f1738a72f4
ramimarogy <ramimarogy@yahoo.com>
2025-03-23 18:53:38 -0400

    added "reset password" feature

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,4 +10,6 @@ router.post("/logout", logout)
 router.post("/verify-email", verifyEmail)
 router.post("/forgot-password", forgotPassword)
 
+router.post("/reset-password/:token", resetPassword)
+
 export default router
